29d85ad85730f094f125653ce8d169116cab66c1
Rafi Fried <rafifried1@gmail.com>
2020-06-10 18:30:19 -0700

    adding hello_world

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 18:25:53 2020
+
+@author: rafifried
+"""
+
+
+def main():
+
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
